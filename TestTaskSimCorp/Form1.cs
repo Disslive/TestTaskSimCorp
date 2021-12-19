3c958ceb7ba78b960ecd78ae1fa805a0ac1703f1
Vladislav Skorniak <skorniakv@gmail.com>
2021-12-19 13:24:23 +0200

    takes agreement date, calculation date, interest rate, invest summ and investement duration to calculate all future interest payments

---
+++ TestTaskSimCorp/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace TestTaskSimCorp
+{
+    public partial class Form1 : Form
+    {
+
+        double intRate, invSumm, paymentAmount, principalAmount, interestAmount, totalPayments, totalInterest;
+        DateTime agreementDate, paymentDate, calculationDate, endDate;
+        int years, paymentNumbers;
+        string message;
+        string[] columnnames = { "PaymentDate", "PaymentAmount", "PrincipalAmount", "InterestAmount" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PaymentCalculations pc = new PaymentCalculations();
+            DataTable table = new DataTable("Result");
+            for (int i = 0; i < columnnames.Length; i++)
+            {
+                table = CreateColumn(table, "System.String", columnnames[i]);
+            }
+
+
+            if (Double.TryParse(textBox1.Text, out intRate) && Double.TryParse(textBox2.Text, out invSumm) && int.TryParse(textBox3.Text, out years))
+            {
+                agreementDate = dateTimePicker1.Value;
+                calculationDate = dateTimePicker2.Value;
+                endDate = agreementDate.AddYears(years);
+                totalPayments = 0;
+                totalInterest = 0;
+                if (endDate > agreementDate && calculationDate > agreementDate && calculationDate < endDate && calculationDate >= agreementDate.AddMonths(1).AddDays(-1))
+                {
+                    intRate = pc.CalculateMonthlyRate(intRate);
+
+
+
+                    paymentNumbers = pc.CalculatePaymentNumbers(agreementDate, calculationDate);
+                    paymentAmount = pc.CalculatePaymentAmount(invSumm, intRate, paymentNumbers);
+                    paymentDate = agreementDate;
+                    totalInterest = invSumm;
+                    while (invSumm != 0)
+                    {
+                        interestAmount = pc.CalculateInterestAmount(invSumm, intRate);
+                        if (invSumm > paymentAmount)
+                        {
+                            principalAmount = Math.Round(paymentAmount - interestAmount, 2);
+                            invSumm = Math.Round(invSumm - principalAmount, 2);
+                            paymentDate = paymentDate.AddMonths(1);
+                        }
+                        else
+                        {
+                            principalAmount = invSumm;
+                            invSumm = 0;
+                            paymentAmount = Math.Round(principalAmount + interestAmount, 2);
+                            paymentDate = calculationDate;
+                        }
+                        totalPayments += paymentAmount;
+                        table = CreateRow(paymentDate, paymentAmount, principalAmount, interestAmount, table, columnnames);
+                    }
+                    totalInterest = Math.Round(totalPayments - totalInterest, 2);
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = table;
+                    label7.Text = $"Total payments: {totalPayments} \nTotal Interest: {totalInterest}";
+                    label7.Visible = true;
+                }
+                else
+                {
+                    message = "Calculation date must be in range of Agreement date and End date(Agreement date + Investment duration).";
+                    MessageOutput(message);
+                }
+            }
+            else
+            {
+                message = "Wrong data in some fields.";
+                MessageOutput(message);
+            }
+
+        }
+
+        private DataTable CreateRow(DateTime paymentDate, double paymentAmount, double principalAmount, double interestAmount, DataTable table, string[] columnnames)
+        {
+            DataRow row = table.NewRow();
+            row[columnnames[0]] = Convert.ToString(paymentDate.ToShortDateString());
+            row[columnnames[1]] = Convert.ToString(paymentAmount);
+            row[columnnames[2]] = Convert.ToString(principalAmount);
+            row[columnnames[3]] = Convert.ToString(interestAmount);
+            table.Rows.Add(row);
+            return table;
+        }
+
+        private DataTable CreateColumn(DataTable table, string dataType, string columnname)
+        {
+            DataColumn column = new DataColumn();
+            column.DataType = Type.GetType(dataType);
+            column.ColumnName = columnname;
+            column.AutoIncrement = false;
+            column.Caption = columnname;
+            column.ReadOnly = false;
+            column.Unique = false;
+            table.Columns.Add(column);
+            return table;
+        }
+        private void MessageOutput(string message) => MessageBox.Show(message, "Error", MessageBoxButtons.OK);
+    }
+}
---
+++ TestTaskSimCorp/PaymentCalculations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TestTaskSimCorp
+{
+    class PaymentCalculations
+    {
+        public double CalculatePaymentAmount(double invSumm, double intRate, int paymentNumbers)
+        {
+            double paymentAmount = invSumm * ((intRate * (Math.Pow(1 + intRate, paymentNumbers))) / (Math.Pow(1 + intRate, paymentNumbers) - 1));
+            return Math.Round(paymentAmount, 2);
+        }
+
+        public double CalculateInterestAmount(double summ, double intRate)
+        {
+            return Math.Round(intRate * summ, 2);
+        }
+
+        public int CalculatePaymentNumbers(DateTime agreementDate, DateTime calculationDate)
+        {
+            int paymentNums = 0;
+            paymentNums = calculationDate.Month - agreementDate.Month;
+            paymentNums += (calculationDate.Year - agreementDate.Year) * 12;
+            return paymentNums;
+        }
+
+
+
+        public double CalculateMonthlyRate(double intRate)
+        {
+            double monthRate = intRate / (12 * 100);
+            return monthRate;
+        }
+
+    }
+}
